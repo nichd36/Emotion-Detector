cfe180e34cdf167d9d0e96c9f731ce56874370f7
Nicholas Dylan <128449429+nichd36@users.noreply.github.com>
2023-11-05 19:51:15 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,26 +20,33 @@ emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutra
 
 # Create a Streamlit window
 st.title("Emotion Detection 😁🥲🥰😖🥺😡")
-
+picture = st.camera_input("Take a picture")
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 # Create a Streamlit loop to display the webcam feed
-if uploaded_image is not None:
+if uploaded_image is not None or picture is not None:
     # Read the uploaded image
-    image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+    if uploaded_image is not None:
+        image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+    else:
+        image = cv2.imdecode(np.fromstring(picture.read(), np.uint8), 1)
     
     # Find haar cascade to draw bounding box around face
     facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        prediction = model.predict(cropped_img)
-        maxindex = int(np.argmax(prediction))
-        cv2.putText(image, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    if len(faces) == 0:
+        st.warning("No faces found in the image.")
+    else:
+        st.success(f"{len(faces)} faces found")        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            cv2.putText(image, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
     # Display the processed image in Streamlit
     st.image(image, channels="BGR", use_column_width=True)
